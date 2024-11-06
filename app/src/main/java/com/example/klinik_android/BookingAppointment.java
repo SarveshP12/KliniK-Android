c3ee7d0cdbcb6334c5d82db2945ab8c15a1e784a
SarveshP12 <sarvesh.2005.1201@gmail.com>
2024-11-07 03:07:31 +0530

    booking appointment

--- app/src/main/java/com/example/klinik_android/BookingAppointment.java
+++ app/src/main/java/com/example/klinik_android/BookingAppointment.java
@@ -1,17 +1,23 @@
 package com.example.klinik_android;
 
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,8 +27,10 @@ public class BookingAppointment extends AppCompatActivity {
     private TextView physidTextView, nameTextView, specialtyTextView, addressTextView, phoneTextView, descriptionTextView;
     private TextView patientName;
     private DatePicker datePicker;
+    private TimePicker timePicker;
     private Button bookAppointmentButton;
     private String physID, doctorName, photoUrl;
+    private boolean status = false;
 
     private FirebaseFirestore db;
 
@@ -42,6 +50,7 @@ public class BookingAppointment extends AppCompatActivity {
         descriptionTextView = findViewById(R.id.descriptionTextView);
         datePicker = findViewById(R.id.calendarView); // Assuming DatePicker is used for date selection
         bookAppointmentButton = findViewById(R.id.bookAppointmentButton);
+        timePicker = findViewById(R.id.timePicker);
 
         patientName = findViewById(R.id.profile_name);
         // Get the physID passed from the DoctorAdapter
@@ -107,22 +116,27 @@ public class BookingAppointment extends AppCompatActivity {
         int month = datePicker.getMonth();
         int year = datePicker.getYear();
 
-        Calendar appointmentDate = Calendar.getInstance();
-        appointmentDate.set(year, month, day);
+        // Retrieve time from TimePicker
+        int hour = timePicker.getHour();
+        int minute = timePicker.getMinute();
 
+        // Set date and time in Calendar instance
+        Calendar appointmentDateTime = Calendar.getInstance();
+        appointmentDateTime.set(year, month, day, hour, minute);
 
         // Prepare data for Firestore
         Map<String, Object> appointmentData = new HashMap<>();
         appointmentData.put("doctorName", doctorName);
         appointmentData.put("patientName", patientName);
-        appointmentData.put("appointmentDate", appointmentDate); // Stores as a Timestamp
-//        appointmentData.put("appointmentSlot");
+        appointmentData.put("appointmentDateTime", appointmentDateTime);
         appointmentData.put("physID", physID);
+        appointmentData.put("status",status);
 
         // Store the appointment data in Firestore under "appointments" collection
         db.collection("appointments").add(appointmentData)
-                .addOnSuccessListener(documentReference ->
-                        Toast.makeText(this, "Appointment booked successfully!", Toast.LENGTH_SHORT).show())
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(this, "Appointment booked successfully!", Toast.LENGTH_SHORT).show();
+                })
                 .addOnFailureListener(e ->
                         Toast.makeText(this, "Failed to book appointment: " + e.getMessage(), Toast.LENGTH_SHORT).show());
     }
